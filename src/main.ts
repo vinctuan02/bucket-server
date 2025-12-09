0addd7b413277a6f614d82b359c1ef05f9cea532
Vin Tuan <vinctuan02@gmail.com>
2025-12-09 21:51:55 +0700

    chore

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api-docs', app, document);
 
-	await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 // Helper function to preserve raw body for webhook signature verification
